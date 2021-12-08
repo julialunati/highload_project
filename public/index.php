187870c2f91337fc7e3fbf62cfbdfd81cf9bdb77
julialunati <julia.molokova11@icloud.com>
2021-12-08 11:32:47 +0100

    using Monolog\Logger

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
+require_once('../vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+$a1=1;
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('log/my.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+
+$log->warning('Memory '.memory_get_usage());
+
 echo phpinfo();
