bfa6719e37d01179b453bdc47db674215626c012
julialunati <julia.molokova11@icloud.com>
2021-12-18 16:02:20 +0100

    dz5

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CacheController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cache',[CacheController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
